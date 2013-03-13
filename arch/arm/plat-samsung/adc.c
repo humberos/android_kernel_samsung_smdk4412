08146795fa8e69ed56348d19980c58823e95980a
Humberto Borba <humberos@gmail.com>
2013-03-13 11:47:10 -0300

    n80xx: update 8
    Signed-off-by: Humberto Borba <humberos@gmail.com>

--- arch/arm/plat-samsung/adc.c
+++ arch/arm/plat-samsung/adc.c
@@ -163,10 +163,8 @@ int s3c_adc_start(struct s3c_adc_client *client,
 
 	BUG_ON(!adc);
 
-	if (client->is_ts && adc->ts_pend) {
-		spin_unlock_irqrestore(&adc->lock, flags);
+	if (client->is_ts && adc->ts_pend)
 		return -EAGAIN;
-	}
 
 	if (atomic_xchg(&client->running, 1)) {
 		WARN(1, "%s: %p is already running\n", __func__, client);
